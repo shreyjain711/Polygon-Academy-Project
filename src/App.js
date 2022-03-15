3c006d4fbb077d504573a41b500f2005a8995b7c
Shrey Jain <shrey.jain711@gmail.com>
2022-03-15 12:10:20 +0530

    apprentice challenge - fix

--- src/App.js
+++ src/App.js
@@ -23,9 +23,7 @@ function App() {
       <Header/>
       <Content>
         <Container text textAlign='center'>
-          <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
-          <p>Create React Ethereum DApp on Polygon</p>
-          <p>Build your first app with Starter Kits</p>
+          <p>hello world</p>
         </Container>
       </Content>
       <Footer/>
